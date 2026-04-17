5a7577992f741c458917aa860e541b5fd0da5443
Bahaa204 <brawass6@gmail.com>
2026-04-17 21:20:25 +0300

    prepping for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -15,7 +15,7 @@ import ProjectsDashboard from "./pages/ProjectsDashboard";
 import ResetPassword from "./pages/ResetPassword";
 import Support from "./pages/Support";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <MainLayout />,
