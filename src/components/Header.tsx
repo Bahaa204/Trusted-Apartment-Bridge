ccb1e073c045f6ca5f90c071524bd95a439faf76
Bahaa204 <brawass6@gmail.com>
2026-04-19 21:53:12 +0300

    refactor: enhance Header component with mobile menu and accessibility improvements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,20 @@
-import { useAuth } from "@/hooks/useAuth";
+import { useState, type MouseEvent } from "react";
+import { Dialog, DialogPanel, PopoverGroup } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link, NavLink, useNavigate } from "react-router-dom";
-import { Button } from "./ui/button";
 import Logo from "/images/NavBarLogo.png";
-import type { MouseEvent } from "react";
-import { useState } from "react";
+import { Button } from "./ui/button";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function Header() {
   const navigate = useNavigate();
-  const [showLogoutNotice, setShowLogoutNotice] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(true);
+  const [showLogoutNotice, setShowLogoutNotice] = useState<boolean>(false);
 
   const NAVLINKS = [
     { to: "/", label: "Home" },
-    { to: "/about", label: "About" },
-    { to: "/contact", label: "Contact" },
+    { to: "/about", label: "About Us" },
+    { to: "/contact", label: "Contact Us" },
     { to: "/projects", label: "Projects" },
     { to: "/support", label: "Support" },
     { to: "/admin", label: "Admin" },
@@ -46,27 +48,42 @@ export default function Header() {
           Logged out successfully
         </div>
       )}
-
-      <header className="sticky top-0 z-5000 bg-white/80 backdrop-blur-md border-b border-gray-100">
-        <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
-          <Link to="/" className="flex items-center gap-3">
-            <img src={Logo} alt="Logo" className="size-18" />
-          </Link>
-
-          <nav className="flex items-center gap-8">
-            {NAVLINKS.map((link) => {
+      <header className="bg-[#e6e0d8] z-5000">
+        <nav
+          aria-label="Global"
+          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        >
+          <div className="flex lg:flex-1">
+            <Link to="/" className="flex items-center gap-3">
+              <span className="sr-only">Trusted Apartment Bridge - TAB</span>
+              <img src={Logo} alt="Logo" className="size-18" />
+            </Link>
+          </div>
+          <div className="flex lg:hidden">
+            <Button
+              type="button"
+              variant="link"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 cursor-pointer"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="size-6" />
+            </Button>
+          </div>
+          <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+            {NAVLINKS.map((link, index) => {
               if (link.label === "Admin" && !condition) return;
 
               return (
                 <NavLink
-                  key={link.to}
+                  key={index}
                   to={link.to}
                   end={link.to === "/"}
                   className={({ isActive }) =>
-                    `text-[14px] font-medium transition ${
+                    `text-sm/6 font-medium transition ${
                       isActive
-                        ? "text-orange-500"
-                        : "text-gray-600 hover:text-orange-500"
+                        ? "text-orange-500 underline decoration-2"
+                        : "text-gray-600 hover:text-orange-500 hover:underline hover:decoration-2"
                     }`
                   }
                 >
@@ -74,27 +91,99 @@ export default function Header() {
                 </NavLink>
               );
             })}
-          </nav>
-          {!Session ? (
-            <Button
-              variant="link"
-              className="cursor-pointer text-[14px]"
-              onClick={() => navigate("/login")}
-              disabled={AuthLoading}
-            >
-              Login
-            </Button>
-          ) : (
-            <Button
-              variant="link"
-              className="cursor-pointer text-[14px]"
-              onClick={handleClick}
-              disabled={AuthLoading}
-            >
-              Logout
-            </Button>
-          )}
-        </div>
+          </PopoverGroup>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            {!Session ? (
+              <Button
+                variant="link"
+                className="cursor-pointer text-[14px] text-gray-600 hover:text-orange-500 hover:underline hover:decoration-2"
+                onClick={() => navigate("/login")}
+                disabled={AuthLoading}
+              >
+                Login
+              </Button>
+            ) : (
+              <Button
+                variant="link"
+                className="cursor-pointer text-[14px] text-gray-600 hover:text-orange-500 hover:underline hover:decoration-2"
+                onClick={handleClick}
+                disabled={AuthLoading}
+              >
+                Logout
+              </Button>
+            )}
+          </div>
+        </nav>
+        <Dialog
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+          className="lg:hidden"
+        >
+          <div className="fixed inset-0 z-5000" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-[#e6e0d8] p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <Link to="/" className="flex items-center gap-3">
+                <span className="sr-only">Trusted Apartment Bridge - TAB</span>
+                <img src={Logo} alt="Logo" className="size-18" />
+              </Link>
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="size-6" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6 flex flex-col">
+                  {NAVLINKS.map((link, index) => {
+                    if (link.label === "Admin" && !condition) return;
+
+                    return (
+                      <NavLink
+                        key={index}
+                        to={link.to}
+                        end={link.to === "/"}
+                        className={({ isActive }) =>
+                          `text-lg font-medium transition ${
+                            isActive
+                              ? "text-orange-500 underline decoration-2"
+                              : "text-gray-600 hover:text-orange-500 hover:underline hover:decoration-2"
+                          }`
+                        }
+                      >
+                        {link.label}
+                      </NavLink>
+                    );
+                  })}
+                </div>
+                <div className="py-6">
+                  {!Session ? (
+                    <Button
+                      variant="link"
+                      className="cursor-pointer text-lg text-gray-600 hover:text-orange-500 hover:underline hover:decoration-2"
+                      onClick={() => navigate("/login")}
+                      disabled={AuthLoading}
+                    >
+                      Login
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="link"
+                      className="cursor-pointer text-lg text-gray-600 hover:text-orange-500 hover:underline hover:decoration-2"
+                      onClick={handleClick}
+                      disabled={AuthLoading}
+                    >
+                      Logout
+                    </Button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </Dialog>
       </header>
     </>
   );
